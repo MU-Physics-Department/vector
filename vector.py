e8a6a9a0d07a78033e09754cf2f74428053acd3a
Saba <34461238+sbbzplt@users.noreply.github.com>
2022-11-14 14:26:25 +0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,1 +1,15 @@
+class Vector:
+    def __init__(self, x: [int, float], y: [int, float], z: [int, float]):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+
 print("Vector module is imported successfully.")
